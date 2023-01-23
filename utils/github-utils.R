5caf81de966e318aef8c978853417eb67cdc2196
gongcastro <gonzalo.garciadecastro@upf.edu>
2023-01-23 10:34:19 +0100

    Update website

--- utils/github-utils.R
+++ utils/github-utils.R
@@ -1,26 +1,40 @@
 gert::git_status()
 
-quarto_push <- function(branch = "test", commit = "Update website") {
-    stopifnot(git_branch_exists(branch))
+quarto_push <- function(branch_name, 
+                        commit_message = NULL,
+                        push = TRUE,
+                        pull_request = TRUE) {
     
-    
-    if (branch=="main") {
-        cli::cli_alert_warning(paste0("Branch set to ", branch))
-        continue <- usethis::ui_yeah("Do you want to continue?")
-    } else {
-        cli_alert_info(paste0("Rendering from branch ", branch))
+    if (git_branch_exists(branch_name)) {
+        cli_abort(paste0("Branch ", branch_name, " already exists. Specify a different name"))
     }
     
-    if (!continue) {
-        ui_stop("Rendering aborted")
-    }
-    
+    pr_init(branch = branch_name)
     
     cli_progress_step("Rendering website...")
-    quarto_render()
+    quarto_render(as_job = FALSE, quiet = TRUE)
     
-    cli_progress_step(paste0("", branch, " branch..."))
-    git_add("docs/")
+    cli_progress_step(paste0("Staging files..."))
+    git_add(".")
     
-    cli_progress_done()
+    cli_progress_step(paste0("Creating commit", branch, "..."))
+    if (is.null(commit_message)) {
+        cli_alert_warning("Commit message is empty")
+        commit_message <- readline("Add a commit message: \n")
+    }
+    commit_tag <- git_commit(commit_message)
+    
+    if (push) {
+        cli_progress_step(paste0("Pushing to ", branch, " branch..."))
+        git_push()
+    } else {
+        cli_progress_done()
+    }
+    
+    if (push & pull_request) {
+        usethis::pr_pull()
+        gert::pu
+    }
+    
+    
 }
