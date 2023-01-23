b170f0ad917e652ca14bcffa3a8a2ca45b895cfa
gongcastro <gonzalo.garciadecastro@upf.edu>
2023-01-23 11:14:44 +0100

    "Change colour of descriptions in posts"

--- utils/github-utils.R
+++ utils/github-utils.R
@@ -1,39 +1,36 @@
-gert::git_status()
+git_new_branch <- function(branch_name) {
+    gert::git_branch_create(branch_name)
+    gert::git_branch_checkout(branch_name)
+    cli::cli_alert_success("Branch {.field {branch_name}} created and checked-out")
+}
 
 quarto_push <- function(branch_name, 
                         commit_message = NULL,
                         push = TRUE,
                         pull_request = TRUE) {
     
-    if (git_branch_exists(branch_name)) {
-        cli_abort(paste0("Branch ", branch_name, " already exists. Specify a different name"))
-    }
-    
-    pr_init(branch = branch_name)
-    
-    cli_progress_step("Rendering website...")
+    cli::cli_progress_step("Rendering website...")
     quarto_render(as_job = FALSE, quiet = TRUE)
     
-    cli_progress_step(paste0("Staging files..."))
-    git_add(".")
+    cli::cli_progress_step(paste0("Staging files..."))
+    gert::git_add(".")
     
-    cli_progress_step(paste0("Creating commit", branch, "..."))
+    cli::cli_progress_step(paste0("Creating commit", branch, "..."))
     if (is.null(commit_message)) {
-        cli_alert_warning("Commit message is empty")
+        cli::cli_alert_warning("Commit message is empty")
         commit_message <- readline("Add a commit message: \n")
     }
-    commit_tag <- git_commit(commit_message)
+    commit_tag <- gert::git_commit(commit_message)
     
     if (push) {
-        cli_progress_step(paste0("Pushing to ", branch, " branch..."))
-        git_push()
+        cli::cli_progress_step(paste0("Pushing to ", branch, " branch..."))
+        gert::git_push()
     } else {
-        cli_progress_done()
+        cli::cli_progress_done()
     }
     
     if (push & pull_request) {
-        usethis::pr_pull()
-        gert::pu
+        usethis::pr_push()
     }
     
     
